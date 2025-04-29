3424cfb008802798ce8c6c42cef89298d61c0bf9
sebastian lapt <smoreira11@hotmail.com>
2025-04-30 08:12:41 +0900

    add new

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,10 +1,18 @@
-﻿int[] inventory = { 200, 450, 700, 175, 250 };
-int sum = 0;
-int bin = 0;
+﻿int[] inventory = {  B123,
+C234,
+A345,
+C15,
+B177,
+G3003,
+C235,
+B179 ,   };
+
 foreach (int items in inventory)
 {
-    sum += items;
-    bin++;
-    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+   
+string name = "Bob";
+if (name.StartsWith("B"))
+{
+    Console.WriteLine("The name starts with 'B'!");
 }
-Console.WriteLine($"We have {sum} items in inventory.");
+}
